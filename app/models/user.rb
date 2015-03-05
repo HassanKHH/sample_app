ca9a896d6b711303dd8e0c72b35218a43afbed72
hassan <hassan_khh99@yahoo.co.jp>
2015-03-05 09:01:50 +0000

    User model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    before_save {self.email = email.downcase}
     validates :name, presence: true, length: {maximum:100} 
     validates :email, presence: true, length: {maximum:200}, format: {with: EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
+                    
+    has_secure_password
+    validates :password, length: { minimum: 6 }
 end
