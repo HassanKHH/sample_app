50e72900d617b561a1bac259c342af21cd8f6008
hassan <hassan_khh99@yahoo.co.jp>
2015-03-18 02:57:34 +0000

    added admin features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only:[:edit,:update,:index]
   before_action :correct_user?, only:[:edit,:update]
+  before_action :admin_user?, only:[:destroy]
+  
   
   def show
     @user = User.find(params[:id])
@@ -33,9 +35,13 @@ class UsersController < ApplicationController
   end
   
   def index
-    @all = User.all
+    @users = User.paginate(page:params[:page])
   end
     
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
     
   
   private
@@ -53,6 +59,12 @@ class UsersController < ApplicationController
     
     def correct_user?
       unless current_user == User.find(params[:id])
+        redirect_to root_url
+      end
+    end
+    
+    def admin_user?
+      unless current_user.admin?
         redirect_to root_url
       end
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name:"Hassan", email:"hassan@example.com",
+password:"password", password_confirmation:"password", admin: true)
+
+100.times do |n|
+    name = Faker::Name.name
+    email = "example" + n.to_s + "@example.com"
+    User.create!(name: name, email: email, password: "password", password_confirmation: "password")
+end
