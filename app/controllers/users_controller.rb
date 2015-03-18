6a4b3385773b7e2494cf52d420a79b04f77fb2c2
hassan <hassan_khh99@yahoo.co.jp>
2015-03-18 01:43:43 +0000

    added index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only:[:edit,:update]
-  
+  before_action :logged_in_user, only:[:edit,:update,:index]
+  before_action :correct_user?, only:[:edit,:update]
   
   def show
     @user = User.find(params[:id])
@@ -31,6 +31,10 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def index
+    @all = User.all
+  end
     
     
   
@@ -44,6 +48,12 @@ class UsersController < ApplicationController
     def logged_in_user
       unless logged_in?
         redirect_to login_url
+      end
+    end
+    
+    def correct_user?
+      unless current_user == User.find(params[:id])
+        redirect_to root_url
       end
     end
 
